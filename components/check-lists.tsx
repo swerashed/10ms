76f7adc6e2df55dcb88836f70ced7569bae02d78
Rashed <rashed@notionhive.com>
2025-07-26 05:34:01 +0600

    feat: media player

---
+++ components/check-lists.tsx
@@ -0,0 +1,14 @@
+const CheckLists = () => {
+  // 1. State hooks
+  // 2. Functions/handlers
+  // 3. useEffect or other hooks
+  // 4. scope component or mini component
+  
+  return (
+    <div>
+      {/* title */}
+    </div>
+  );
+};
+
+export default CheckLists;
