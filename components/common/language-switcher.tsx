20efb3c664645e4cd72b893d87e8f455fc779ffa
Rashed <rashed@notionhive.com>
2025-07-28 07:04:24 +0600

    fix: responsive issue

--- components/common/language-switcher.tsx
+++ components/common/language-switcher.tsx
@@ -19,7 +19,7 @@ const LanguageSwitcher = () => {
         <div className="relative z-50">
             <button
                 onClick={handleLanguageSwitch}
-                className="flex items-center gap-2 rounded-md border h-full border-border bg-white px-3 py-1 text-sm text-gray-700 hover:bg-gray-50"
+                className="flex cursor-pointer items-center gap-2 rounded-md border h-full border-border bg-white px-3 py-1 text-sm text-gray-700 hover:bg-gray-50"
             >
                 <LanguageIcon />
                 <span>{targetLang.toUpperCase()}</span>
